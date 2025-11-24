24860b7156cfe54ea24b3c42e5c01003668b95ce
master <mymail@google.com>
2025-11-24 11:15:44 +0900

    first commit

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+require("dotenv").config({ path: "./database/dbConfig.env" });
+const express = require("express");
+const app = express();
+const port = 3000;
+
+console.log(process.env.DB_NAME);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/hello", (req, res) => {
+  console.log(req.url);
+  res.send("Hello World!");
+});
+
+//운영 모드에서 추가적인 경로 설정
+let apiPath = "";
+if (process.argv[2] == "prod") {
+  apiPath = "/api";
+}
+
+app.get(`${apiPath}/board`, (req, res) => {
+  res.send({ title: "노드 api 서버 update!!!!" });
+});
+
+const path = require("path");
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
+//위에선언된 모든 라우터에게 적용 [가장 마지막 코드로 존재해야 함]
+// 새로고침 에러 예외처리
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public", "index.html"));
+});
